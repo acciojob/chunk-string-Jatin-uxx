e845883886d6e42d1fb016f1fca2bfa669dc1ba6
Jatin-uxx <rawatjatin2001@gmail.com>
2025-01-03 00:08:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function stringChop(str, size) {
   // your code here
+	let btr = [];
+ if(str.length<=0)
+{
+    return btr
+}
+
+
+	
+
+for (let i = 0; i < str.length; i += size) {
+  let j = i + size; 
+  btr.push(str.slice(i, j)); 
+}
+
+return btr
 }
 
 // Do not change the code below
